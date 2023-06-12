2b3a0af363b4d87a4c7cfb3403a23dc99935aa10
Luis Escobar <luisaeniebla@gmail.com>
2023-06-12 11:08:15 -0700

    90% finished with hero
    Almost done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import "./Home.scss";
+import Featured from '../../components/featured/Featured';
+import TrustedBy from '../../components/trustedBy/TrustedBy';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+      <Featured />
+      <TrustedBy />
+    </div>
   )
 }
 
