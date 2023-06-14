1bee8936ecaf3203ee1a733209a9f7035f9c6756
Luis Escobar <luisaeniebla@gmail.com>
2023-06-13 21:53:56 -0700

    MyGigs + Orders + Messages sections completed
    Next one is the singular message section that uses real time chatting

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,9 +1,76 @@
 import React from 'react';
 import "./Messages.scss";
+import { Link } from 'react-router-dom';
 
 const Messages = () => {
+
+  const currentUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true, 
+  };
+
+  const message = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, nulla beatae? At laborum vitae eveniet. Expedita quam aliquam ipsum, commodi dicta praesentium dolorum laboriosam, quo deserunt harum, non itaque incidunt?'
+
   return (
-    <div>Messages</div>
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+          
+        </div>
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr className="active">
+            <td>
+              John Doe
+            </td>
+            <td><Link to="/message/123" className='link'>{message.substring(0,100)}... </Link></td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr className='active'>
+            <td>
+              John Doe
+            </td>
+            <td><Link to="/message/123" className='link'>{message.substring(0,100)}... </Link></td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              John Doe
+            </td>
+            <td><Link to="/message/123" className='link'>{message.substring(0,100)}... </Link></td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>
+              John Doe
+            </td>
+            <td><Link to="/message/123" className='link'>{message.substring(0,100)}... </Link></td>
+            <td>1 day ago</td>
+          </tr>
+          <tr>
+            <td>
+              John Doe
+            </td>
+            <td><Link to="/message/123" className='link'>{message.substring(0,100)}... </Link></td>
+            <td>1 day ago</td>
+          </tr>
+          
+        </table>
+      </div>
+    </div>
   )
 }
 
