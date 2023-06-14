bf9413623b9a326dd620addbee5f9852e397b57a
Luis Escobar <luisaeniebla@gmail.com>
2023-06-14 13:04:56 -0700

    add page and chat page complete
    All aesthetics done, next time we will be going into the back end

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import "./Message.scss";
+import { Link } from 'react-router-dom';
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className="message">
+      <div className="container">
+        <span className="breadcrumbs">
+          <Link to="/messages">MESSAGES</Link> > JOHN DOE > 
+        </span>
+
+        <div className="messages">
+          <div className="item">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident corporis sed vero accusamus eius neque commodi. Nemo nisi dignissimos ipsa alias suscipit ipsam distinctio enim? Sit porro voluptatum blanditiis perspiciatis?</p>
+            
+          </div>
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident corporis sed vero accusamus eius neque commodi. Nemo nisi dignissimos ipsa alias suscipit ipsam distinctio enim? Sit porro voluptatum blanditiis perspiciatis?</p>
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident corporis sed vero accusamus eius neque commodi. Nemo nisi dignissimos ipsa alias suscipit ipsam distinctio enim? Sit porro voluptatum blanditiis perspiciatis?</p>
+            
+          </div>
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident corporis sed vero accusamus eius neque commodi. Nemo nisi dignissimos ipsa alias suscipit ipsam distinctio enim? Sit porro voluptatum blanditiis perspiciatis?</p>
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident corporis sed vero accusamus eius neque commodi. Nemo nisi dignissimos ipsa alias suscipit ipsam distinctio enim? Sit porro voluptatum blanditiis perspiciatis?</p>
+            
+          </div>
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident corporis sed vero accusamus eius neque commodi. Nemo nisi dignissimos ipsa alias suscipit ipsam distinctio enim? Sit porro voluptatum blanditiis perspiciatis?</p>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="write">
+          <textarea name="" placeholder="write a message" id="" cols="30" rows="10"></textarea>
+          <button>Send</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
